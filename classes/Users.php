e8ec586961b51dfe86e05b2a49cb517c247d558e
PythonScript1 <hplaptopsimo@gmail.com>
2023-12-19 16:26:34 +0100

    adding register and login methods for Users.php

--- classes/Users.php
+++ classes/Users.php
@@ -5,6 +5,45 @@ class Users extends Database
 {
     private $usersTable = "hd_users";
 
+    /** SignUp USER:
+     * @param $data
+     * @return bool
+     */
+    public function register($data){
+        $this->db->query('INSERT INTO users (name, email, password) VALUES(:name, :email, :password)');
+        // Bind values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', $data['password']);
+
+        // Execute
+        if($this->db->execute()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /** Login USER:
+     * @param $email
+     * @param $password
+     * @return false
+     */
+    public function login($email, $password){
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->single();
+
+        $hashed_password = $row->password;
+        if(password_verify($password, $hashed_password)){
+            return $row;
+        } else {
+            return false;
+        }
+    }
+
+
     /** List All USERS:
      * @return mixed
      */
